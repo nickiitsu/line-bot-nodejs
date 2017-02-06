4e6f1f69fd532cc9f3536ea1e46b892b0efcf242
infernalslam <infernal-slam@hotmail.com>
2017-02-07 06:12:20 +0700

    Show reply token

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-var linebot = require('linebot')
+var request = require('request')
 var app = express()
+/*eslint-disable */
+var env = require('dotenv').config({ path: __dirname + '/.env' })
+/*eslint-enable */
+console.log(process.env.CHANNEL_ACCESS_TOKEN)
 
 app.use(bodyParser.json())
 
@@ -9,26 +13,14 @@ app.set('port', (process.env.PORT || 4000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-const bot = linebot({
-  channelId: process.env.CHANNEL_ID,
-  channelSecret: process.env.CHANNEL_SECRET,
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
-})
-
 app.post('/webhook', function (req, res) {
+  console.log('show reply token :::::', req.body.events[0].replyToken)
   console.log('send from userId :::::::::::', req.body.events[0].message.text)
   console.log('sender source ::::', req.body.events[0].source.userId)
-  // console.log('text :::: send form User :::', req.body.events.message.text)
   res.sendStatus(200)
 })
 
-bot.on('message', function (event) {
-  event.reply(event.message.text).then(function (data) {
-    console.log('Success', data)
-  }).catch(function (error) {
-    console.log('Error', error)
-  })
-})
+// function sendTextMessege () {}
 
 app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'))
