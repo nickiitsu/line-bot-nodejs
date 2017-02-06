b9b6c6d81a9c3693d6864f553d44cb8c50840c26
infernalslam <infernal-slam@hotmail.com>
2017-02-07 04:29:08 +0700

    webhook 102

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
 var express = require('express')
-// var bodyParser = require('body-parser')
+var bodyParser = require('body-parser')
 var app = express()
 var linebot = require('linebot')
 /*eslint-disable */
 var env = require('dotenv').config({ path: __dirname + '/.env' })
 /*eslint-enable */
-// app.use(bodyParser.json())
 app.set('port', (process.env.PORT || 4000))
-// app.use(bodyParser.urlencoded({extended: false}))
-// app.use(bodyParser.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+const parser = bodyParser.json({
+  verify: function (req, res, buf, encoding) {
+    req.rawBody = buf.toString(encoding)
+  }
+})
 
 const bot = linebot({
   channelId: process.env.CHANNEL_ID,
   channelSecret: process.env.CHANNEL_SECRET,
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  verify: true
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 })
 
-const linebotParser = bot.parser()
-app.post('/webhook', linebotParser)
-
-bot.on('message', function (event) {
-  event.reply(event.message.text).then(function (data) {
-    console.log('Success', data)
-  }).catch(function (error) {
-    console.log('Error', error)
-  })
+app.post('/linewebhook', parser, function (req, res) {
+  if (!bot.verify(req.rawBody, req.get('X-Line-Signature'))) {
+    return res.sendStatus(400)
+  }
+  bot.parse(req.body)
+  return res.json({})
 })
 
 app.listen(app.get('port'), function () {
