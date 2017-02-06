6059e1b494aa04b37a94d627bf57c392b024c841
infernalslam <infernal-slam@hotmail.com>
2017-02-07 05:08:34 +0700

    Add function event linebot

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var linebot = require('linebot')
 var app = express()
 
 app.use(bodyParser.json())
@@ -10,6 +11,20 @@ app.use(bodyParser.json())
 
 app.post('/webhook', function (req, res) {
   res.send('connection complete')
+})
+
+const bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+})
+
+bot.on('message', function (event) {
+  event.reply(event.message.text).then(function (data) {
+    console.log('Success', data)
+  }).catch(function (error) {
+    console.log('Error', error)
+  })
 })
 
 app.listen(app.get('port'), function () {
