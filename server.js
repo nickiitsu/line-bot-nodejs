47d71432dc0735d332af84c3ae9e1385a36430d4
infernalslam <infernal-slam@hotmail.com>
2017-02-07 04:23:49 +0700

    test webhook

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 var express = require('express')
-var bodyParser = require('body-parser')
+// var bodyParser = require('body-parser')
 var app = express()
 var linebot = require('linebot')
 /*eslint-disable */
 var env = require('dotenv').config({ path: __dirname + '/.env' })
 /*eslint-enable */
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 app.set('port', (process.env.PORT || 4000))
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({extended: false}))
+// app.use(bodyParser.json())
 
 const bot = linebot({
   channelId: process.env.CHANNEL_ID,
   channelSecret: process.env.CHANNEL_SECRET,
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  verify: true
 })
 
 const linebotParser = bot.parser()
