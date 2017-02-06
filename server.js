f3d47a29bc1825aed3221b6b64efcf8460ad92f3
infernalslam <infernal-slam@hotmail.com>
2017-02-07 06:28:11 +0700

    sendtext

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var app = express()
 /*eslint-disable */
 var env = require('dotenv').config({ path: __dirname + '/.env' })
 /*eslint-enable */
-console.log(process.env.CHANNEL_ACCESS_TOKEN)
 
 app.use(bodyParser.json())
 
@@ -14,13 +13,40 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.post('/webhook', function (req, res) {
-  console.log('show reply token :::::', req.body.events[0].replyToken)
-  console.log('send from userId :::::::::::', req.body.events[0].message.text)
-  console.log('sender source ::::', req.body.events[0].source.userId)
+  // console.log('show reply token :::::', req.body.events[0].replyToken)
+  // console.log('send from userId :::::::::::', req.body.events[0].message.text)
+  // console.log('sender source ::::', req.body.events[0].source.userId)
+  var text = req.body.events[0].message.text
+  var sender = req.body.events[0].source.userId
+  var replyToken = req.body.events[0].replyToken
+  if (text === 'สวัสดี') {
+    sendTextMessege(sender, text, replyToken)
+  }
   res.sendStatus(200)
 })
 
-// function sendTextMessege () {}
+function sendTextMessege (sender, text, replyToken) {
+  var data = {
+    replyToken: replyToken,
+    messages: [
+      {
+        type: 'text',
+        text: 'เราชื่อหนูหนค่ะ พี่เเต๊กสุดหล่อ'
+      }
+    ]
+  }
+  request({
+    headers: {
+      Authorization: 'Bearer{' + process.env.CHANNEL_ACCESS_TOKEN + '}'
+    },
+    url: 'https://api.line.me/v2/bot/message/reply',
+    method: 'POST',
+    body: data
+  }, function (err, res, body) {
+    if (err) console.log(err)
+    if (res) console.log(res)
+  })
+}
 
 app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'))
