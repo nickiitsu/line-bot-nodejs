74378df192245c2122b791b36b32101a9b2d2a31
infernalslam <infernal-slam@hotmail.com>
2017-02-07 19:30:47 +0700

    add

--- server.js
+++ server.js
@@ -1,128 +1,26 @@
-const linebot = require('../index.js');
+var express = require('express')
+var bodyParser = require('body-parser')
+// var request = require('request')
+var app = express()
+/*eslint-disable */
+var env = require('dotenv').config({ path: __dirname + '/.env' })
+/*eslint-enable */
 
-const bot = linebot({
-	channelId: process.env.CHANNEL_ID,
-	channelSecret: process.env.CHANNEL_SECRET,
-	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-	verify: true // default=true
-});
+app.use(bodyParser.json())
 
-bot.on('message', function (event) {
-	switch (event.message.type) {
-		case 'text':
-			switch (event.message.text) {
-				case 'Me':
-					event.source.profile().then(function (profile) {
-						return event.reply('Hello ' + profile.displayName + ' ' + profile.userId);
-					});
-					break;
-				case 'Picture':
-					event.reply({
-						type: 'image',
-						originalContentUrl: 'https://d.line-scdn.net/stf/line-lp/family/en-US/190X190_line_me.png',
-						previewImageUrl: 'https://d.line-scdn.net/stf/line-lp/family/en-US/190X190_line_me.png'
-					});
-					break;
-				case 'Location':
-					event.reply({
-						type: 'location',
-						title: 'LINE Plus Corporation',
-						address: '1 Empire tower, Sathorn, Bangkok 10120, Thailand',
-						latitude: 13.7202068,
-						longitude: 100.5298698
-					});
-					break;
-				case 'Push':
-					bot.push('U6350b7606935db981705282747c82ee1', ['Hey!', 'สวัสดี ' + String.fromCharCode(0xD83D, 0xDE01)]);
-					break;
-				case 'Confirm':
-					event.reply({
-						type: 'template',
-						altText: 'this is a confirm template',
-						template: {
-							type: 'confirm',
-							text: 'Are you sure?',
-							actions: [{
-								type: 'message',
-								label: 'Yes',
-								text: 'yes'
-							}, {
-								type: 'message',
-								label: 'No',
-								text: 'no'
-							}]
-						}
-					});
-					break;
-				case 'Multiple':
-					return event.reply(['Line 1', 'Line 2', 'Line 3', 'Line 4', 'Line 5']);
-					break;
-				case 'Version':
-					event.reply('linebot@' + require('../package.json').version);
-					break;
-				default:
-					event.reply(event.message.text).then(function (data) {
-						console.log('Success', data);
-					}).catch(function (error) {
-						console.log('Error', error);
-					});
-					break;
-			}
-			break;
-		case 'image':
-			event.message.content().then(function (data) {
-				const s = data.toString('base64').substring(0, 30);
-				return event.reply('Nice picture! ' + s);
-			}).catch(function (err) {
-				return event.reply(err.toString());
-			});
-			break;
-		case 'video':
-			event.reply('Nice movie!');
-			break;
-		case 'audio':
-			event.reply('Nice song!');
-			break;
-		case 'location':
-			event.reply(['That\'s a good location!', 'Lat:' + event.message.latitude, 'Long:' + event.message.longitude]);
-			break;
-		case 'sticker':
-			event.reply({
-				type: 'sticker',
-				packageId: 1,
-				stickerId: 1
-			});
-			break;
-		default:
-			event.reply('Unknow message: ' + JSON.stringify(event));
-			break;
-	}
-});
+app.set('port', (process.env.PORT || 4000))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
-bot.on('follow', function (event) {
-	event.reply('follow: ' + event.source.userId);
-});
+app.post('/webhook', (req, res) => {
+  var text = req.body.events[0].message.text
+  var sender = req.body.events[0].source.userId
+  var replyToken = req.body.events[0].replyToken
+  console.log(text, sender, replyToken)
+  console.log(req.body.events[0])
+  res.sendStatus(200)
+})
 
-bot.on('unfollow', function (event) {
-	event.reply('unfollow: ' + event.source.userId);
-});
-
-bot.on('join', function (event) {
-	event.reply('join: ' + event.source.groupId);
-});
-
-bot.on('leave', function (event) {
-	event.reply('leave: ' + event.source.groupId);
-});
-
-bot.on('postback', function (event) {
-	event.reply('postback: ' + event.postback.data);
-});
-
-bot.on('beacon', function (event) {
-	event.reply('beacon: ' + event.beacon.hwid);
-});
-
-bot.listen('/linewebhook', process.env.PORT || 80, function () {
-	console.log('LineBot is running.');
-});
+app.listen(app.get('port'), function () {
+  console.log('run at port', app.get('port'))
+})
