49c311de8a8c75c21efbd1c6b17da57272ef8fb0
infernalslam <infernal-slam@hotmail.com>
2017-02-07 21:58:21 +0700

    no message

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-// var request = require('request')
+var request = require('request')
 var app = express()
 /*eslint-disable */
 var env = require('dotenv').config({ path: __dirname + '/.env' })
@@ -18,8 +18,39 @@ app.post('/webhook', (req, res) => {
   var replyToken = req.body.events[0].replyToken
   console.log(text, sender, replyToken)
   console.log(req.body.events[0])
+  if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
+    replyMessage(sender, text)
+  }
   res.sendStatus(200)
 })
+
+function replyMessage (sender, text) {
+  let data = {
+    to: sender,
+    messages: [
+      {
+        type: 'text',
+        text: 'สวัสดีค่ะ เราเป็นผู้ช่วยปรึกษาด้านความรัก สำหรับหมามิ้น 💞'
+      },
+      {
+        type: 'text',
+        text: text
+      }
+    ]
+  }
+  request({
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + process.env.CHANNEL_ACCESS_TOKEN
+    },
+    url: 'https://api.line.me/v2/bot/message/reply',
+    method: 'POST',
+    json: data
+  }, function (err, res, body) {
+    if (err) console.log(err)
+    if (res) console.log(res)
+  })
+}
 
 app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'))
