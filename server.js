317e729fa71db3727f368777c07192098fc5c5ab
infernalslam <infernal-slam@hotmail.com>
2017-02-07 07:44:13 +0700

    Refashr

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-var request = require('request')
-// var https = require('https')
+// var request = require('request')
+var https = require('https')
 var app = express()
 /*eslint-disable */
 var env = require('dotenv').config({ path: __dirname + '/.env' })
@@ -10,7 +10,7 @@ var env = require('dotenv').config({ path: __dirname + '/.env' })
 app.use(bodyParser.json())
 
 app.set('port', (process.env.PORT || 4000))
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 app.post('/webhook', function (req, res) {
@@ -26,16 +26,16 @@ app.post('/webhook', function (req, res) {
   res.sendStatus(200)
 })
 
-// var options = {
-//   host: 'api.line.me',
-//   // port: 443,
-//   path: '/v2/bot/message/reply',
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'Authorization': 'Bearer ' + process.env.CHANNEL_ACCESS_TOKEN
-//   }
-// }
+var options = {
+  host: 'api.line.me',
+  // port: 443,
+  path: '/v2/bot/message/reply',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + process.env.CHANNEL_ACCESS_TOKEN
+  }
+}
 
 function sendTextMessege (sender, text, replyToken) {
   console.log('text ::: ', text, 'sender ::: ', sender)
@@ -48,29 +48,30 @@ function sendTextMessege (sender, text, replyToken) {
       }
     ]
   }
-  // let rplyJson = JSON.stringify(data)
-  // var request = https.request(options, function (response) {
-  //   console.log('Status: ' + response.statusCode)
-  //   console.log('Headers: ' + JSON.stringify(response.headers))
-  //   response.setEncoding('utf8')
-  // })
-  // request.on('error', function (e) {
-  //   console.log('Request error: ' + e.message)
-  // })
-  // request.end(rplyJson)
-
-  request({
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: 'Bearer  ' + process.env.CHANNEL_ACCESS_TOKEN
-    },
-    url: 'https://api.line.me/v2/bot/message/reply',
-    method: 'POST',
-    body: data
-  }, function (err, res, body) {
-    if (err) console.log(err)
-    if (res) console.log(res)
+  let rplyJson = JSON.stringify(data)
+  var request = https.request(options, function (response) {
+    console.log('Status: ' + response.statusCode)
+    console.log('Headers: ' + JSON.stringify(response.headers))
+    response.setEncoding('utf8')
   })
+  request.on('error', function (e) {
+    console.log('Request error: ' + e.message)
+  })
+  request.end(rplyJson)
+
+  // request({
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'Authorization': 'Bearer  ' + process.env.CHANNEL_ACCESS_TOKEN
+  //   },
+  //   url: 'https://api.line.me/v2/bot/message/reply',
+  //   method: 'POST',
+  //   json: true,
+  //   body: data
+  // }, function (err, res, body) {
+  //   if (err) console.log(err)
+  //   if (res) console.log(res)
+  // })
 }
 
 app.listen(app.get('port'), function () {
