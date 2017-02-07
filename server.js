e19223c20273aa0d5755ad45318235a9f55e5096
infernalslam <infernal-slam@hotmail.com>
2017-02-07 08:46:57 +0700

    add

--- server.js
+++ server.js
@@ -1,7 +1,41 @@
-import Bot, { Messages } from 'node-line-messaging-api'
+var express = require('express')
+var bodyParser = require('body-parser')
+// var request = require('request')
+// var https = require('https')
+var linebot = require('linebot')
+var app = express()
 /*eslint-disable */
 var env = require('dotenv').config({ path: __dirname + '/.env' })
-const PORT = process.env.PORT || 3002
-let bot = new Bot(process.env.CHANNEL_SECRET, process.env.CHANNEL_ACCESS_TOKEN, { webhook: { port: PORT } })
+/*eslint-enable */
 
-bot.on('webhook', w => console.log(`bot listens on port ${w}.`))
+app.use(bodyParser.json())
+
+app.set('port', (process.env.PORT || 4000))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+var bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+})
+
+app.post('/webhook', (req, res) => {
+  var text = req.body.events[0].message.text
+  var sender = req.body.events[0].source.userId
+  var replyToken = req.body.events[0].replyToken
+  console.log(text, sender, replyToken)
+
+  bot.on('message', function (event) {
+    event.reply(event.message.text).then(function (data) {
+      console.log(data)
+    }).catch(function (error) {
+      console.log(error)
+    })
+  })
+  res.sendStatus(200)
+})
+
+app.listen(app.get('port'), function () {
+  console.log('run at port', app.get('port'))
+})
