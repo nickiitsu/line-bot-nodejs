4f90852586c2a6d6a6270f4a4e247b3dcbda676e
nickiitsu <pannatorn.po@skru.ac.th>
2017-08-30 09:30:08 +0700

    Update server.js

--- server.js
+++ server.js
@@ -55,18 +55,13 @@ app.post('/webhook', (req, res) => {
       "actions": [
           {
             "type": "postback",
-            "label": "Buy",
-            "data": "action=buy&itemid=123"
-          },
-          {
-            "type": "postback",
-            "label": "Add to cart",
-            "data": "action=add&itemid=123"
-          },
+            "label": "ดู PDF",
+            "data": "https://passport.skru.ac.th/evaluate/manual/manual_staff_ver.1.pdf"
+          },        
           {
             "type": "uri",
-            "label": "View detail",
-            "uri": "http://example.com/page/123"
+            "label": "ดูรูป",
+            "uri": "https://passport.skru.ac.th/evaluate/SKRU_Line_Bot/server/php/files/p2.jpg"
           }
       ]
   }
